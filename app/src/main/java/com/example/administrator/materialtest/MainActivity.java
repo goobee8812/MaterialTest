dde61d3fcfd6a1e0a01f2955de477e3b36618cbe
goobee <goobee_yuer0716@163.com>
2017-05-04 17:39:12 +0800

    完成floatactionbar

--- app/src/main/java/com/example/administrator/materialtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/materialtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.administrator.materialtest;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         NavigationView navView = (NavigationView)findViewById(R.id.nav_view);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -52,6 +56,18 @@ public class MainActivity extends AppCompatActivity {
                 }
                 drawerLayout.closeDrawers();
                 return true;
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Data deleted",Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this,"Data restored",Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
             }
         });
     }
