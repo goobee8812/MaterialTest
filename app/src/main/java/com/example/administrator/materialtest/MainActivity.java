e6c5c0e310712e01371e540e3746c6acbc9303d5
goobee <goobee_yuer0716@163.com>
2017-05-04 11:55:45 +0800

    完成ToolBar

---
+++ app/src/main/java/com/example/administrator/materialtest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.administrator.materialtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"You Click Backup",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"You Click Delete",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"You Click Settings",Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+}
